d2e350117dd4bed11fd8718d567e6c261582bd89
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2026-02-20 23:50:12 -0600

    Cree una pagina llamada hola

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route} from 'react-router-dom'
 import Index from './pages/Index'
 import ConsultarEmpleados from './pages/ConsultarEmpleados'
 import RegistrarEmpleado from './pages/RegistrarEmpleado'
+import Expediente from './pages/Expediente'
 
 function App() {
 
@@ -12,8 +13,10 @@ function App() {
       }
       <Route path='/administrador/consultarEmpleados' element={<ConsultarEmpleados/>}/>
       <Route path='/administrador/registrarEmpleado' element={<RegistrarEmpleado/>}/>
+      <Route path='/loQuieran' element={<Expediente/>}/>
     </Routes>
   )
 }
+
 
 export default App
---
+++ src/pages/Expediente.jsx
@@ -0,0 +1,13 @@
+import "./../styles/expediente.css"
+import Header from "./components/Header"
+
+function Expediente() {
+    return (
+        <>
+            <Header/>
+            <div>Expediente</div>
+        </>
+    )
+}
+
+export default Expediente
