ad3e338049f4788026e577ad39fdb8fc216f4b3e
Ramses <ramsessldlng@gmail.com>
2026-02-23 18:45:37 -0600

    Registro con formik

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route} from 'react-router-dom'
 import Index from './pages/Index'
 import ConsultarEmpleados from './pages/ConsultarEmpleados'
 import RegistrarEmpleado from './pages/RegistrarEmpleado'
+import Login from './pages/Login'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       }
       <Route path='/administrador/consultarEmpleados' element={<ConsultarEmpleados/>}/>
       <Route path='/administrador/registrarEmpleado' element={<RegistrarEmpleado/>}/>
+      <Route path='/administrador/Login' element={<Login/>}/>
     </Routes>
   )
 }
