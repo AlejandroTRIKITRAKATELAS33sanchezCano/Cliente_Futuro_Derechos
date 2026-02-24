907b0fa59d01a5afb709ce37cfe900de2fe31e82
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2026-02-24 11:57:04 -0600

    Login como Index

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route} from 'react-router-dom'
-import Index from './pages/Index'
 import ConsultarEmpleados from './pages/ConsultarEmpleados'
 import RegistrarEmpleado from './pages/RegistrarEmpleado'
 import Login from './pages/Login'
@@ -8,12 +7,11 @@ function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Index/>}/>
+      <Route path='/' element={<Login/>}/>
       {//Rutas Administrador
       }
       <Route path='/administrador/consultarEmpleados' element={<ConsultarEmpleados/>}/>
       <Route path='/administrador/registrarEmpleado' element={<RegistrarEmpleado/>}/>
-      <Route path='/Login' element={<Login/>}/>
     </Routes>
   )
 }
