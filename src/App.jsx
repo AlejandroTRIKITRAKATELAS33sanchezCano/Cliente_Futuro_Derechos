b0519e90e51bfce63bec4b9d8ea7921c863180d4
Cano's Code  (；●∀●)ゝ” <99841453+AlejandroTRIKITRAKATELAS33sanchezCano@users.noreply.github.com>
2026-02-23 19:43:21 -0600

    Neta ya registro con formik | Consultar Usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import { Routes, Route} from 'react-router-dom'
 import Index from './pages/Index'
 import ConsultarEmpleados from './pages/ConsultarEmpleados'
 import RegistrarEmpleado from './pages/RegistrarEmpleado'
-<<<<<<< HEAD
-import Hola from './pages/Hola'
-=======
-import Login from './pages/Login'
->>>>>>> Ramses
 
 function App() {
 
@@ -17,7 +12,6 @@ function App() {
       }
       <Route path='/administrador/consultarEmpleados' element={<ConsultarEmpleados/>}/>
       <Route path='/administrador/registrarEmpleado' element={<RegistrarEmpleado/>}/>
-      <Route path='/Hola' element={<Hola/>}/>
     </Routes>
   )
 }
